060239ac5a625197810c4ba727ad245db4d2a2f9
yangtaoliu <yangtaoliu@outlook.com>
2019-04-21 21:31:56 +0800

    add time format in note list display

--- lib/insta_list.dart
+++ lib/insta_list.dart
@@ -4,6 +4,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import "package:async/src/async_memoizer.dart";
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:intl/intl.dart';
 import 'file_pperation.dart';
 
 
@@ -40,153 +41,162 @@ class _InstaList extends State<InstaList> {
       if(FileOperation.noteDataList.noteList.length > 0){
         return new ListView.builder(
           itemCount: FileOperation.noteDataList.noteList.length + 1,
-          itemBuilder: (context, index) => index == 0
-          ? Text(
-                  "My Stories:",
-                  style: TextStyle(fontSize: 16.0 ,fontWeight: FontWeight.bold),
-                ) /* new SizedBox(
-              child: new InstaStories(),
-              height: deviceSize.height * 0.15,
-            ) */
-          :new Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              FileOperation.noteDataList.noteList[FileOperation.noteDataList.noteNum-index].imagePath.isNotEmpty
-              ?Container(
-                //fit: FlexFit.loose,
-                height: 300,
-                child: FileOperation.noteDataList.noteList[FileOperation.noteDataList.noteNum-index].imagePath.length==1
-                ?new Image.file(
-                  new File(FileOperation.noteDataList.noteList[FileOperation.noteDataList.noteNum-index].imagePath[0]),
-                  filterQuality:FilterQuality.low ,
-                  fit: BoxFit.fitWidth, 
-                  scale: 0.1,                
+          itemBuilder: (context, index) {
+            
+          if(index == 0){
+            return Text(
+                    "My Stories:",
+                    style: TextStyle(fontSize: 16.0 ,fontWeight: FontWeight.bold),
+                  ); /* new SizedBox(
+                child: new InstaStories(),
+                height: deviceSize.height * 0.15,
+              ) */
+          }
+          else{
+            int rsvOrder = FileOperation.noteDataList.noteNum-index;
+            NoteData noteContent = FileOperation.noteDataList.noteList[rsvOrder];
+            String timeFolding =readTimestamp(noteContent.datetime);
+            return new Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                noteContent.imagePath.isNotEmpty
+                ?Container(
+                  //fit: FlexFit.loose,
+                  height: 300,
+                  child: noteContent.imagePath.length==1
+                  ?new Image.file(
+                    new File(noteContent.imagePath[0]),
+                    filterQuality:FilterQuality.low ,
+                    fit: BoxFit.fitWidth, 
+                    scale: 0.1,                
+                  )
+                  :Swiper(
+                    itemBuilder: (BuildContext context, int swipIdx) {
+                      int itemNum = noteContent.imagePath.length;
+                      debugPrint("Swiper itemNum:$itemNum");
+                      return new Image.file(
+                        File(noteContent.imagePath[swipIdx]),
+                        filterQuality:FilterQuality.low ,
+                        fit: BoxFit.fitWidth,
+                        scale: 0.1,             
+                      );
+                    },
+                    itemCount: noteContent.imagePath.length,
+                    viewportFraction: 0.8,
+                    scale: 0.9,
+                    pagination: new SwiperPagination(),
+                    onTap: (index) => print('点击了第$index个'),
+                  )
                 )
-                :Swiper(
-                  itemBuilder: (BuildContext context, int swipIdx) {
-                    int itemNum = FileOperation.noteDataList.noteList[FileOperation.noteDataList.noteNum-index].imagePath.length;
-                    debugPrint("Swiper itemNum:$itemNum");
-                    return new Image.file(
-                      new File(FileOperation.noteDataList.noteList[FileOperation.noteDataList.noteNum-index].imagePath[swipIdx]),
-                      filterQuality:FilterQuality.low ,
-                      fit: BoxFit.fitWidth,
-                      scale: 0.1,             
-                    );
-                  },
-                  itemCount: FileOperation.noteDataList.noteList[FileOperation.noteDataList.noteNum-index].imagePath.length,
-                  viewportFraction: 0.8,
-                  scale: 0.9,
-                  pagination: new SwiperPagination(),
-                  onTap: (index) => print('点击了第$index个'),
-                )
-              )
-              :Padding(
-                //当没有图片时候，给一个空的占位元素
-                padding: const EdgeInsets.all(0.0),
-                child:Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children:<Widget>[Text('')]),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    new Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        new Icon(
-                          FontAwesomeIcons.heart,
-                        ),
-                        new SizedBox(
-                          width: 16.0,
-                        ),
-                        new Icon(
-                          FontAwesomeIcons.comment,
-                        ),
-                        new SizedBox(
-                          width: 16.0,
-                        ),
-                        new Icon(FontAwesomeIcons.paperPlane),
-                      ],
-                    ),
-                    new Icon(Icons.check_circle,color:(FileOperation.noteDataList.noteList[FileOperation.noteDataList.noteNum-index].postID==null
-                    ||FileOperation.noteDataList.noteList[FileOperation.noteDataList.noteNum-index].postID=="") 
-                    ? Colors.grey : Colors.black,)
-                  ],
+                :Padding(
+                  //当没有图片时候，给一个空的占位元素
+                  padding: const EdgeInsets.all(0.0),
+                  child:Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children:<Widget>[Text('')]),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 14.0),
-                child: Text(
-                  FileOperation.noteDataList.noteList[FileOperation.noteDataList.noteNum-index].feeling,
-                  style: TextStyle(fontWeight: FontWeight.normal),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                child: new Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                  Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Row(
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      new Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: <Widget>[
                           new Icon(
-                            Icons.location_on,
+                            FontAwesomeIcons.heart,
                           ),
-                          Expanded(
-                            child:Text(FileOperation.noteDataList.noteList[FileOperation.noteDataList.noteNum-index].location.isEmpty
-                            ?'没有位置信息哟...':FileOperation.noteDataList.noteList[FileOperation.noteDataList.noteNum-index].location,
-                              style: TextStyle(fontWeight: FontWeight.w100),softWrap: true),
-                        ),
-                      ]),
+                          new SizedBox(
+                            width: 16.0,
+                          ),
+                          new Icon(
+                            FontAwesomeIcons.comment,
+                          ),
+                          new SizedBox(
+                            width: 16.0,
+                          ),
+                          new Icon(FontAwesomeIcons.paperPlane),
+                        ],
+                      ),
+                      new Icon(Icons.check_circle,color:(noteContent.postID==null
+                      ||noteContent.postID=="") 
+                      ? Colors.grey : Colors.black,)
+                    ],
                   ),
-                  ]),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(16.0, 16.0, 0.0, 8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    new Container(
-                      height: 40.0,
-                      width: 40.0,
-                      child: new Image.asset(
-                          'assets/images/avatar.jpg',
-                          width: 40,
-                          height: 40,
-                          fit: BoxFit.cover,
-                      ),
-                    ),
-                    new SizedBox(
-                      width: 10.0,
-                    ),
-                    Expanded(
-                      child: new TextField(
-                        decoration: new InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "想说点什么...",
-                        ),
-                        onChanged: (text) {
-                          debugPrint('submcommentString index: $index');
-                          commentStr = text;
-                        },
-                      ),
-                    ),
-                  ],
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child:Text("Some Days Ago", style: TextStyle(color: Colors.grey)),
-              )
-            ],
-          ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 14.0),
+                  child: Text(
+                    noteContent.feeling,
+                    style: TextStyle(fontWeight: FontWeight.normal),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: new Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                    Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Row(
+                          children: <Widget>[
+                            new Icon(
+                              Icons.location_on,
+                            ),
+                            Expanded(
+                              child:Text(noteContent.location.isEmpty
+                              ?'没有位置信息哟...':noteContent.location,
+                                style: TextStyle(fontWeight: FontWeight.w100),softWrap: true),
+                          ),
+                        ]),
+                    ),
+                    ]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16.0, 16.0, 0.0, 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      new Container(
+                        height: 40.0,
+                        width: 40.0,
+                        child: new Image.asset(
+                            'assets/images/avatar.jpg',
+                            width: 40,
+                            height: 40,
+                            fit: BoxFit.cover,
+                        ),
+                      ),
+                      new SizedBox(
+                        width: 10.0,
+                      ),
+                      Expanded(
+                        child: new TextField(
+                          decoration: new InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "想说点什么...",
+                          ),
+                          onChanged: (text) {
+                            debugPrint('submcommentString index: $index');
+                            commentStr = text;
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child:Text(timeFolding, style: TextStyle(color: Colors.grey)),
+                )
+              ],
+            );
+            };
+          }
         );
       }else{
         return new Stack(
@@ -239,6 +249,36 @@ class _InstaList extends State<InstaList> {
         cnt++;
       }
     );
+  }
+  String readTimestamp(String timesStr) {
+    if(timesStr == null || timesStr == ''){
+      return 'Some time ago';
+    }
+    int dealTime = DateTime.parse(timesStr).millisecondsSinceEpoch;
+    var now = new DateTime.now();
+    var format = new DateFormat('HH:mm a');
+    var date = new DateTime.fromMillisecondsSinceEpoch(dealTime);
+    var diff = now.difference(date);
+    var timeStr = '';
+
+    if (diff.inSeconds <= 0 || diff.inSeconds > 0 && diff.inMinutes == 0 || diff.inMinutes > 0 && diff.inHours == 0 || diff.inHours > 0 && diff.inDays == 0) {
+      timeStr = 'Today ' + format.format(date);
+    } else if (diff.inDays > 0 && diff.inDays < 7) {
+      if (diff.inDays == 1) {
+        timeStr = diff.inDays.toString() + ' Day ago';
+      } else {
+        timeStr = diff.inDays.toString() + ' Days ago';
+      }
+    } else {
+      if (diff.inDays == 7) {
+        timeStr = (diff.inDays / 7).floor().toString() + ' Week ago';
+      } else {
+
+        timeStr = (diff.inDays / 7).floor().toString() + ' Weeks ago';
+      }
+    }
+
+    return timeStr;
   }
 }
 
